723c8e8938592552818d912c43673f945b8a5d3b
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-04 22:45:23 +0800

    Update A_Normal_Theory.js

--- A_Normal_Theory.js
+++ A_Normal_Theory.js
@@ -8,7 +8,7 @@ import { Utils } from "../api/Utils";
 var id = "Nothin";
 var name = "A normal theory";
 var description = "This theory is normal. Your goal is to ██████. Thats cool! GLHF ";
-var version = "Alpha 0.0.1";
+var version = 0.0.1;
 
 var currency;
 var a1, b1;
@@ -24,16 +24,21 @@ var init = () => {
   // Regular Upgrades
 
   // a1
-  let getDesc = (level) => "a_1=" + getA1(level).toString(0);
-  a1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(1, Math.log2(2))));
-  a1.getDescription = (_) => Utils.getMath(getDesc(a1.level));
-  a1.getInfo = (amount) => Utils.getMathTo(getDesc(a1.level), getDesc(a1.level + amount));
+  {
+    let getDesc = (level) => "a_1=" + getA1(level).toString(0);
+    a1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(1, Math.log2(2))));
+    a1.getDescription = (_) => Utils.getMath(getDesc(a1.level));
+    a1.getInfo = (amount) => Utils.getMathTo(getDesc(a1.level), getDesc(a1.level + amount));
+  }
   
   // b1
-  let getDesc = (level) => "b_1=" + getB1(level).toString(0);
-  b1 = theory.createUpgrade(1, currency, new FirstFreeCost(new ExponentialCost(5, Math.log2(12))));
-  b1.getDescription = (_) => Utils.getMath(getDesc(b1.level));
-  b1.getInfo = (amount) => Utils.getMathTo(getDesc(b1.level), getDesc(b1.level + amount));
+  {
+    let getDesc = (level) => "b_1=" + getB1(level).toString(0);
+    b1 = theory.createUpgrade(1, currency, new FirstFreeCost(new ExponentialCost(5, Math.log2(12))));
+    b1.getDescription = (_) => Utils.getMath(getDesc(b1.level));
+    b1.getInfo = (amount) => Utils.getMathTo(getDesc(b1.level), getDesc(b1.level + amount));
+  }
+  
   
   /////////////////////
   // Permanent Upgrades
