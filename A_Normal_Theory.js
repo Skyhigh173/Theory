70fd9f0a07b0f5ed33683b693b428050dc39b515
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-05 14:56:09 +0800

    Update A_Normal_Theory.js

--- A_Normal_Theory.js
+++ A_Normal_Theory.js
@@ -105,5 +105,9 @@ var getPrimaryEquation = () => {
 var getA1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
 var getB1 = (level) => BigNumber.TWO.pow(level);
 
+var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
+var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
+var getTau = () => currency.value;
+var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 
 init();
