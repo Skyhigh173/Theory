419245fb2923303fa474b71dc02acf195ebd4fe5
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-12 15:21:07 +0800

    Update complex-theory.js

--- OFFICAL/complex-theory.js
+++ OFFICAL/complex-theory.js
@@ -233,7 +233,7 @@ var getTertiaryEquation = () => {
     result += " \\qquad  "
     if (UpOTerm.level > 0) {
         result += "\\Omega = ";
-        result += (BigNumber.from( UpOmega )).toString(4) ;
+        result += (BigNumber.from( UpOmega )) ;
     }
     return result;
 }
