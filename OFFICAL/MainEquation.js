1a8c7dbfcccf7e2a25c8ef938eca88714473f5f9
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-17 15:55:27 +0800

    Update MainEquation.js

--- OFFICAL/MainEquation.js
+++ OFFICAL/MainEquation.js
@@ -10,6 +10,31 @@ var description = "Your first theory";
 var authors = "Gilles-Philippe Paillé \n Recreate:Skyhigh173";
 var version = "v1.0.0";
 
-var ft;
+var ft, μ, ψ;
 var c1, c2;
-var c1Exp, c2Exp;
+var x, y, z;
+
+var init = () => {
+    ft = theory.createCurrency("ft", "f(t)");
+    μ = theory.createCurrency("μ", "\\mu");
+    ψ = theory.createCurrency("ψ", "\\psi");
+    
+    ////////////////////////////////////////////////
+    
+    ///////////////////
+    // Regular Upgrades
+    
+    // x
+    {
+        let getDesc = (level) => "x = y + " + getX_Lv(level).toString(0);
+        x = theory.createUpgrade(0, ft, new FreeCost());
+        x.getDescription = (_) => Utils.getMath(getDesc(x.level));
+        x.getInfo = (amount) => Utils.getMathTo(getDesc(x.level), getDesc(x.level + amount));
+    }
+    // y
+    
+}
+
+
+var getX_Lv = (level) => BigNumber.from(level / 10);
+var getX = (level) => BigNumber.from(getY(y.level) + level / 10);
