6d00db918c43d0789c13687a09f21ca3b4badaa6
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-17 19:34:02 +0800

    Update MainEquation.js

--- OFFICAL/MainEquation.js
+++ OFFICAL/MainEquation.js
@@ -26,15 +26,36 @@ var init = () => {
     
     // x
     {
-        let getDesc = (level) => "x = y + " + getX_Lv(level).toString(0);
+        let getDesc = (level) => {
+         
+            let result = "x = y + ";
+            let XLV = getX_Lv(level).toString(0);
+            if (XLV >= 50) {
+                result += "2^{3} \\times";
+            } else if (XLV >= 25) {
+                result += "2^{2} \\times";
+            } else if (XLV >= 10) {
+                result += "2 \\times";
+            }
+
+            result += XLV;
+            return result;
+        }
         x = theory.createUpgrade(0, ft, new FreeCost());
         x.getDescription = (_) => Utils.getMath(getDesc(x.level));
         x.getInfo = (amount) => Utils.getMathTo(getDesc(x.level), getDesc(x.level + amount));
     }
     // y
+    {
+        let getDesc = (level) => "y = z + " + getY_Lv(level).toString(0);
+        y = theory.createUpgrade(0, ft, new FreeCost());
+        y.getDescription = (_) => Utils.getMath(getDesc(y.level));
+        y.getInfo = (amount) => Utils.getMathTo(getDesc(y.level), getDesc(y.level + amount));
+    }
     
 }
 
 
 var getX_Lv = (level) => BigNumber.from(level / 10);
 var getX = (level) => BigNumber.from(getY(y.level) + level / 10);
+var getY_Lv = (level) => BigNumber.from(
