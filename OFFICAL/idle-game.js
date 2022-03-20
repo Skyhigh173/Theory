3fb395b49a5c138361642ad0c8786807bd510109
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-20 21:49:28 +0800

    Update idle-game.js

--- OFFICAL/idle-game.js
+++ OFFICAL/idle-game.js
@@ -81,7 +81,7 @@ var getTau = () => currency.value;
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 
 function getPubPerSecMulti (plus) {
-    return plus * theory.getPublicationMultiplier();
+    return plus * getPublicationMultiplier();
 }
 
 var getA1 = (level) => BigNumber.from(level * 0.1);
