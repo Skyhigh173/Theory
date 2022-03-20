c27ee64835b2a512b3a68842de85a3c9e5907cd4
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-20 21:10:13 +0800

    Update idle-game.js

--- OFFICAL/idle-game.js
+++ OFFICAL/idle-game.js
@@ -33,7 +33,7 @@ var init = () => {
     //a2
     {
         let getDesc = (level) => "a_2" + getA2(level).toString(0);
-        a2 = theory.createUpgrade(0, currency, new ExponentialCost(500, Math.log2(3)));
+        a2 = theory.createUpgrade(1, currency, new ExponentialCost(500, Math.log2(3)));
         a2.getDescription = (_) => Utils.getMath(getDesc(a2.level));
         a2.getInfo = (amount) => Utils.getMathTo(getDesc(a2.level), getDesc(a2.level + amount));
         a2.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
