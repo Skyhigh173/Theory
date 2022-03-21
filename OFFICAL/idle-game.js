9bf4264554e9523fe4be50152fd51e906c420387
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-21 16:12:06 +0800

    Update idle-game.js

--- OFFICAL/idle-game.js
+++ OFFICAL/idle-game.js
@@ -24,17 +24,17 @@ var init = () => {
     
     //a1 
     {
-        let getDesc = (level) => "a_1=" + (a1.level / 2);
+        let getDesc = (level) => "a_1=" + (a1.level);
         a1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(1, Math.log2(2))));
         a1.getDescription = (_) => Utils.getMath(getDesc(a1.level));
-        a1.getInfo = (amount) => "+ " + getPubPerSecMulti(0.5) + " /sec";
+        a1.getInfo = (amount) => "+ " + getPubPerSecMulti(1) + " /sec";
         a1.boughtOrRefunded = (_) => { theory.invalidatePrimaryEquation(); updateAvailability(); };
     }
        
     //a2
     {
         let getDesc = (level) => "a_2=" + (10 * a2.level);
-        a2 = theory.createUpgrade(1, currency, new ExponentialCost(500, Math.log2(2.5)));
+        a2 = theory.createUpgrade(1, currency, new ExponentialCost(300, Math.log2(2.5)));
         a2.getDescription = (_) => Utils.getMath(getDesc(a2.level));
         a2.getInfo = (amount) => "+ " + getPubPerSecMulti(10) + " /sec";
         a2.boughtOrRefunded = (_) => { theory.invalidatePrimaryEquation(); updateAvailability(); };
@@ -42,11 +42,20 @@ var init = () => {
     //a3
     {
         let getDesc = (level) => "a_3=" + (80 * a3.level);
-        a3 = theory.createUpgrade(2, currency, new ExponentialCost(6000, Math.log2(5)));
+        a3 = theory.createUpgrade(2, currency, new ExponentialCost(2000, Math.log2(5)));
         a3.getDescription = (_) => Utils.getMath(getDesc(a3.level));
         a3.getInfo = (amount) => "+ " + getPubPerSecMulti(80) + " /sec";
         a3.boughtOrRefunded = (_) => { theory.invalidatePrimaryEquation(); updateAvailability(); };
         a3.isAvailable = false;
+    }
+    //a4
+    {
+        let getDesc = (level) => "a_4=" + (560 * a4.level);
+        a4 = theory.createUpgrade(3, currency, new ExponentialCost(40000, Math.log2(6)));
+        a4.getDescription = (_) => Utils.getMath(getDesc(a4.level));
+        a4.getInfo = (amount) => "+ " + getPubPerSecMulti(560) + " /sec";
+        a4.boughtOrRefunded = (_) => { theory.invalidatePrimaryEquation(); updateAvailability(); };
+        a4.isAvailable = false;
     }
     
     /////////////////////
@@ -69,13 +78,14 @@ var updateAvailability = () => {
     Auto.isAvailable = a2.level > 100;
     
     a3.isAvailable = a2.level >= 4;
+    a4.isAvailable = a3.level >= 5;
 }
 
 var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
     
-    let TotalA = getA1(a1.level) + getA2(a2.level) + getA3(a3.level);
+    let TotalA = getA1(a1.level) + getA2(a2.level) + getA3(a3.level) + getA4(a4.level);
     currency.value += bonus * dt * TotalA;
 }
 
@@ -95,8 +105,9 @@ function getPubPerSecMulti (plus) {
     return BigNumber.from(plus);
 }
 
-var getA1 = (level) => BigNumber.from(level * 0.1);
+var getA1 = (level) => BigNumber.from(level);
 var getA2 = (level) => BigNumber.from(level * 10);
 var getA3 = (level) => BigNumber.from(level * 80);
+var getA4 = (level) => BigNumber.from(level * 560);
 
 init();
