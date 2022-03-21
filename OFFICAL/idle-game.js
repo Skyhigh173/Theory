806f3d454beccabf5cf614edd2f09e2064e54558
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-21 17:27:20 +0800

    Update idle-game.js

--- OFFICAL/idle-game.js
+++ OFFICAL/idle-game.js
@@ -141,15 +141,7 @@ function getPubPerSecMulti (plus) {
 }
 function getBUnlockInfo (level) {
     //these code is to see if level is 1, or 2, or somethin
-    switch (level) {
-        case 1:
-            return 700000;
-            break;
-        case 2:
-            return 1e10;
-            break;
-          
-    }
+    return 600000;
 }
     
 
