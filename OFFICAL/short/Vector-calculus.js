c9a1892c0dc0615ec8835f6c5463bde1404ea912
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-24 20:13:34 +0800

    Create Vector-calculus.js

---
+++ OFFICAL/short/Vector-calculus.js
@@ -0,0 +1,11 @@
+import { ExponentialCost, FirstFreeCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "Vector_Calculus"
+var name = "Vector Calculus";
+var description = "A theory about Euclidean space.";
+var authors = "Skyhigh173";
+var version = "v0.0.1";
