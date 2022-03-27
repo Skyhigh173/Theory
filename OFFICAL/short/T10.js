98e103d976bbe3ae5c9277740726f5ca3b4a4c75
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-27 16:35:09 +0800

    Create T10.js

---
+++ OFFICAL/short/T10.js
@@ -0,0 +1,15 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { BigNumber, parseBigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "T10"
+var name = "T10";
+var description = "more fun equation, just like t9";
+var authors = "Skyhigh173";
+var version = 1;
+
+var Lemma;
+var b11, b12, a11, a12, q11, TotalUpgrade1;
+
