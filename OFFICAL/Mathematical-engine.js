123e32b0c029199a0310247b28b4178536acf7ab
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-30 15:21:20 +0800

    Create Mathematical-engine.js

---
+++ OFFICAL/Mathematical-engine.js
@@ -0,0 +1,14 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "Mathematical-enginouo";
+var name = "Mathematical Engine";
+var description = "a super idle game";
+var authors = "sky";
+var version = 1;
+
+
+var currency;
