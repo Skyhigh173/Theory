c0951f3654619b99344680ca96b2524dc5dfc938
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-03-30 15:28:07 +0800

    Create Multilinear-multiplication.js

---
+++ OFFICAL/short/Multilinear-multiplication.js
@@ -0,0 +1,14 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+
+var id = "Multilinear-multiplication";
+var name = "Multilinear multiplication";
+var description = "Multi";
+var authors = "Sky";
+var version = 1;
+
+var currency;
