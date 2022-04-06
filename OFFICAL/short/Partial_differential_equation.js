3a3151374969232c6398f34f89e080b00255778e
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-06 21:04:37 +0800

    Update Partial_differential_equation.js

--- OFFICAL/short/Partial_differential_equation.js
+++ OFFICAL/short/Partial_differential_equation.js
@@ -73,7 +73,7 @@ var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
     
-    U += dt * BigNumber.from( getC(c.level) * ( getX(x.level) + getY(y.level) + getZ(z.level) );
+    U += dt * BigNumber.from( getC(c.level) * ( getX(x.level) + getY(y.level) + getZ(z.level ) ) );
     currency.value += U / getC(c.level).pow(2);
 }
 
