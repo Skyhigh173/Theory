23058fc1b64ab15121886f25b579bd92d1ac4f88
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-12 15:15:58 +0800

    Update Partial_differential_equation.js

--- OFFICAL/short/Partial_differential_equation.js
+++ OFFICAL/short/Partial_differential_equation.js
@@ -8,7 +8,7 @@ var id = "PDE";
 var name = "Partial differential equation";
 var description = "partial differential equation (PDE) is an equation which imposes relations between the various partial derivatives of a multivariable function.";
 var authors = "Skyhigh173";
-var version = 2;
+var version = 3;
 
 var c, x, y, z;
 var dp;
@@ -19,6 +19,8 @@ var U = BigNumber.ZERO;
 var currency;
 
 var ac1, ac2, ac3;
+var acS1;
+// acSN = ach special {N}
 
 var init = () => {
     currency = theory.createCurrency();
@@ -61,12 +63,12 @@ var init = () => {
     /////////////////////
     // Permanent Upgrades
     theory.createPublicationUpgrade(0, currency, 1e8);
-    theory.createBuyAllUpgrade(1, currency, 1e13);
+    theory.createBuyAllUpgrade(1, currency, 1e15);
     theory.createAutoBuyerUpgrade(2, currency, 1e30);
     
     {
-        pubM = theory.createPermanentUpgrade(3, currency, new ExponentialCost(1e20, Math.log2(24)));
-        pubM.getDescription = (_) => " $\\uparrow$ Pub multiplier by 0.5";
+        pubM = theory.createPermanentUpgrade(3, currency, new ExponentialCost(1e20, Math.log2(50)));
+        pubM.getDescription = (_) => " $\\uparrow$ Pub multiplier by 0.25";
         pubM.getInfo = (amount) => "Increases Pub multiplier";
     }
     
@@ -81,11 +83,30 @@ var init = () => {
     theory.createAchievement(4, ac1, "Potato factory", "Reach 1e15 rho", () => currency.value >= 1e15);
     theory.createAchievement(5, ac1, "Another^2 start ", "Reach 1e30 rho", () => currency.value >= 1e30);
     theory.createAchievement(6, ac1, "gas gas gas", "Reach 1e50 rho", () => currency.value >= 1e50);
+    theory.createAchievement(7, ac1, "king!", "Reach 1e75 rho", () => currency.value >= 1e75);
+    theory.createAchievement(8, ac1, "Is this the end?", "Reach 1e100 rho", () => currency.value >= 1e100);
     
-    ac2 = theory.createAchievementCategory(1, "Shadow");
+    ac2 = theory.createAchievementCategory(1, "Upgrade");
+    // milestone or upgrade
+    theory.createAchievement(100, ac2, "Equation upgrade", "buy a int upgrade", () => DPT.level >= 1);
+    theory.createAchievement(101, ac2, "exponents^2", "buy a exp upgrade", () => EXP3.level >= 1);
+    theory.createAchievement(102, ac2, "exponents^10", "buy 3 exp upgrade", () => EXP3.level >= 3);
+    theory.createAchievement(103, ac2, "exponents^1024", "buy 6 exp upgrade", () => EXP3.level >= 6);
+    theory.createAchievement(104, ac2, "More exponents!", "Buy two C exp upgrade", () => UEXP.level >= 2);
+    theory.createAchievement(105, ac2, "Better luck this time", "Buy a perm upgrade", () => PERM.level >= 1);
+    
+    ac3 = theory.createAchievementCategory(5, "Shadow");
     // these are shadow achievement. They are hard/need guide to reach but they are NOT secret ach
-    theory.createAchievement(100, ac2, "Why you need to stop?", "variable z is greater than 1000", () => getZ(z.level) >= 1000);
-    theory.createAchievement(101, ac2, "We cant use that thing", "reach 1e10 rho without variable y", () => y.level == 0 && currency.value >= 1e10);
+    theory.createAchievement(1000, ac3, "Why dont you stop?", "variable z is greater than 1 million", () => getZ(z.level) >= 1000000);
+    theory.createAchievement(1001, ac3, "We cant use that thing", "reach 1e10 rho without variable y", () => y.level == 0 && currency.value >= 1e10);
+    theory.createAchievement(1002, ac3, "OCD", "have variable (x/y/z) level same while level is greater than 100", () => x.level == y.level && y.level == z.level && y.level >= 100);
+    
+    acS1 = theory.createAchievementCategory(10, "Luck (Others)");
+    theory.createAchievement(5000, acS1, "Lucky", "you have 1/500 chance (sec) to get this achievement", () => Math.random() <= (1 / 500));
+    theory.createAchievement(5001, acS1, "Super Luck", "you have 1/5000 chance (sec) to get this achievement", () => Math.random() <= (1 / 5000));
+    theory.createAchievement(5002, acS1, "Ultra Luck", "you have 1/10000 chance (sec) to get this achievement", () => Math.random() <= (1 / 10000));
+    theory.createAchievement(5003, acS1, "ULTRA SUPER luck", "you have 1/1000000 chance (sec) to get this achievement", () => Math.random() <= (1 / 1000000));
+    theory.createAchievement(5005, acS1, "ok, you win", "you have 1/1b chance (sec) to get this achievement", () => Math.random() <= (1 / 1000000000));
     
     ///////////////////////
     //// Milestone Upgrades
@@ -233,8 +254,8 @@ var getSecondaryEquation = () => {
 
 var getTertiaryEquation = () => theory.latexSymbol + "=\\max\\rho^{0.1} \\qquad u =" + BigNumber.from(U);
 
-var getPublicationMultiplier = (tau) => tau.pow(2) / BigNumber.TEN * BigNumber.from(1 + pubM.level);
-var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{2}}{10} \\times " + (1 + pubM.level / 2);
+var getPublicationMultiplier = (tau) => tau.pow(1.96) / BigNumber.TEN * BigNumber.from(1 + pubM.level / 4);
+var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{1.96}}{10} \\times " + (1 + pubM.level / 4);
 var getTau = () => currency.value.pow(0.1);
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 
