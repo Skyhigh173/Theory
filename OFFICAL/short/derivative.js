651e985100d434add1dda9f748c6c025edc47d6c
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-17 18:45:26 +0800

    Create derivative.js

---
+++ OFFICAL/short/derivative.js
@@ -0,0 +1,14 @@
+import { ExponentialCost, FirstFreeCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "derivative"
+var name = "Derivative";
+var description = "derivative and derivative, also derivative";
+var authors = "skyhigh173";
+var version = 1;
+
+var currency;
+var D1, D2, D3, D4, D5;
