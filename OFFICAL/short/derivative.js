f9121e5d4837a6f9340eeaf56162ab3e904557bf
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-17 18:51:03 +0800

    Update derivative.js

--- OFFICAL/short/derivative.js
+++ OFFICAL/short/derivative.js
@@ -12,3 +12,18 @@ var version = 1;
 
 var currency;
 var D1, D2, D3, D4, D5;
+
+var init = () => {
+    currency = theory.createCurrency();
+
+    ///////////////////
+    // Regular Upgrades
+
+    // D1
+    {
+        let getDesc = (level) => "\\dot{q}_1=" + getDQ1(level).toString(0) + "\\times q_2";
+        let getInfo = (level) => "\\dot{q}_1=" + (getDQ1(level) * q2).toString();
+        D1 = theory.createUpgrade(0, currency, new FreeCost());
+        D1.getDescription = (amount) => Utils.getMath(getDesc(dq1.level));
+        D1.getInfo = (amount) => Utils.getMathTo(getInfo(dq1.level), getInfo(dq1.level + amount));
+    }
