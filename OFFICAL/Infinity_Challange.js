c7fd5ccf9d55a8972e1e4bbee3b4c98124af05ab
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-19 20:20:53 +0800

    Create Infinity_Challange.js

---
+++ OFFICAL/Infinity_Challange.js
@@ -0,0 +1,21 @@
+import { ExponentialCost, FreeCost, LinearCost } from "./api/Costs";
+import { Localization } from "./api/Localization";
+import { BigNumber } from "./api/BigNumber";
+import { theory } from "./api/Theory";
+import { Utils } from "./api/Utils";
+import { Popup } from "../api/ui/Popup";
+import { Color } from "../api/ui/properties/Color";
+import { ImageSource } from "../api/ui/properties/ImageSource";
+import { Thickness } from "../api/ui/properties/Thickness";
+import { ui } from "../api/ui/UI"
+
+
+var id = "UITest";
+var name = "infinity";
+var description = "A basic theory.";
+var authors = "sky =(";
+var version = 1;
+
+var getUpgradeListDelegate = () => {
+    
+}
