c36ee7afd9927bacdab2f3f0bff05dad9e5aec4d
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-19 20:45:21 +0800

    Update Infinity_Challange.js

--- OFFICAL/Infinity_Challange.js
+++ OFFICAL/Infinity_Challange.js
@@ -18,10 +18,16 @@ var version = 1;
 
 var getUpgradeListDelegate = () => {
     grid = ui.createGrid({
-                columnDefinitions: ["40*", "auto"],
+                columnDefinitions: ["1*", "50"],
                 children: [
                     ui.createButton({text: "Challenge1", row: 0, column: 0, onClicked: () => Empty()}),
                     ui.createButton({text: "Challenge2", row: 0, column: 1, onClicked: () => Empty()}),
+                    ui.createButton({text: "Challenge3", row: 1, column: 0, onClicked: () => Empty()}),
+                    ui.createButton({text: "Challenge4", row: 1, column: 1, onClicked: () => Empty()}),
+                    ui.createButton({text: "Challenge5", row: 2, column: 0, onClicked: () => Empty()}),
+                    ui.createButton({text: "Challenge6", row: 2, column: 1, onClicked: () => Empty()}),
+                    ui.createButton({text: "Challenge7", row: 3, column: 0, onClicked: () => Empty()}),
+                    ui.createButton({text: "Challenge8", row: 3, column: 1, onClicked: () => Empty()})
                 ]
             })
     return grid;
