0d6b6c6baaf0bf9778877d23a0309758359726df
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-19 18:27:19 +0800

    Update Infinity-layers.js

--- OFFICAL/short/Infinity-layers.js
+++ OFFICAL/short/Infinity-layers.js
@@ -22,7 +22,7 @@ var init = () => {
     // a1
     {
         let getDesc = (level) => " \\alpha_1=" + getA1(level).toString(0);
-        a1 = theory.createUpgrade(0, currency1, new FirstFreeCost(new ExponentialCost(10, Math.log2(1.985))));
+        a1 = theory.createUpgrade(0, currency1, new FirstFreeCost(new ExponentialCost(10, Math.log2(2.5))));
         a1.getDescription = (_) => Utils.getMath(getDesc(a1.level));
         a1.getInfo = (amount) => Utils.getMathTo(getDesc(a1.level), getDesc(a1.level + amount));
     }
@@ -30,16 +30,24 @@ var init = () => {
     // a2
     {
         let getDesc = (level) => " \\alpha_2=" + getA2(level).toString(0);
-        a2 = theory.createUpgrade(1, currency1, new ExponentialCost(1e10, Math.log2(2.3)));
+        a2 = theory.createUpgrade(1, currency1, new ExponentialCost(1e200, Math.log2(3.6)));
         a2.getDescription = (_) => Utils.getMath(getDesc(a2.level));
         a2.getInfo = (amount) => Utils.getMathTo(getDesc(a2.level), getDesc(a2.level + amount));
     }
     
+    // a3
+    {
+        let getDesc = (level) => " \\alpha_3=" + getA3(level).toString(0);
+        a3 = theory.createUpgrade(1, currency1, new ExponentialCost(BigNumber.from("ee8"), 100));
+        a3.getDescription = (_) => Utils.getMath(getDesc(a3.level));
+        a3.getInfo = (amount) => Utils.getMathTo(getDesc(a3.level), getDesc(a3.level + amount));
+    }
+    
     /////////////////////
     // Permanent Upgrades
-    theory.createPublicationUpgrade(0, currency1, 1e100);
-    theory.createBuyAllUpgrade(1, currency1, BigNumber.from("1e1000"));
-    theory.createAutoBuyerUpgrade(2, currency1, BigNumber.from("ee4"));
+    theory.createPublicationUpgrade(0, currency1, BigNumber.from("ee4"));
+    theory.createBuyAllUpgrade(1, currency1, BigNumber.from("ee20"));
+    theory.createAutoBuyerUpgrade(2, currency1, BigNumber.from("ee1000"));
     
     updateAvailability();
 }
@@ -49,7 +57,7 @@ var updateAvailability = () => {
 var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
-    currency1.value += getA1(a1.level) + getA2(a2.level);
+    currency1.value += getA1(a1.level);
 }
 
 var getPrimaryEquation = () => {
@@ -64,8 +72,8 @@ var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.00
 var getTau = () => currency1.value;
 var get2DGraphValue = () => currency1.value.sign * (BigNumber.ONE + currency1.value.abs()).log10().toNumber();
 
-var getA1 = (level) => Utils.getStepwisePowerSum(level, 8, 2, 0) + BigNumber.from(a2.level * BigNumber.from("1e300"));
-var getA2 = (level) => Utils.getStepwisePowerSum(level, 120, 2, 0);
-
+var getA1 = (level) => Utils.getStepwisePowerSum(level, 8, 2, 0) + BigNumber.from(getA2(a2.level));
+var getA2 = (level) => Utils.getStepwisePowerSum(level, 120, 2, 0) + BigNumber.from(getA3(a3.level));
+var getA3 = (level) => Utils.getStepwisePowerSum(level, 1e16, 2, 0);
 
 init();
