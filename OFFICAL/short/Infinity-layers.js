44262a8f4ab268537f18a3078003239df202c338
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-19 18:37:05 +0800

    Update Infinity-layers.js

--- OFFICAL/short/Infinity-layers.js
+++ OFFICAL/short/Infinity-layers.js
@@ -22,7 +22,7 @@ var init = () => {
     // a1
     {
         let getDesc = (level) => " \\alpha_1=" + getA1(level).toString(0);
-        a1 = theory.createUpgrade(0, currency1, new FirstFreeCost(new ExponentialCost(10, Math.log2(2.5))));
+        a1 = theory.createUpgrade(0, currency1, new FirstFreeCost(new ExponentialCost(10, Math.log2(3.2))));
         a1.getDescription = (_) => Utils.getMath(getDesc(a1.level));
         a1.getInfo = (amount) => Utils.getMathTo(getDesc(a1.level), getDesc(a1.level + amount));
     }
@@ -30,7 +30,7 @@ var init = () => {
     // a2
     {
         let getDesc = (level) => " \\alpha_2=" + getA2(level).toString(0);
-        a2 = theory.createUpgrade(1, currency1, new ExponentialCost(1e200, Math.log2(3.6)));
+        a2 = theory.createUpgrade(1, currency1, new ExponentialCost(1e300, Math.log2(4)));
         a2.getDescription = (_) => Utils.getMath(getDesc(a2.level));
         a2.getInfo = (amount) => Utils.getMathTo(getDesc(a2.level), getDesc(a2.level + amount));
     }
@@ -38,9 +38,17 @@ var init = () => {
     // a3
     {
         let getDesc = (level) => " \\alpha_3=" + getA3(level).toString(0);
-        a3 = theory.createUpgrade(2, currency1, new ExponentialCost(BigNumber.from("ee8"), 100));
+        a3 = theory.createUpgrade(2, currency1, new ExponentialCost(BigNumber.from("ee8.5"), 100));
         a3.getDescription = (_) => Utils.getMath(getDesc(a3.level));
         a3.getInfo = (amount) => Utils.getMathTo(getDesc(a3.level), getDesc(a3.level + amount));
+    }
+    
+    // a4
+    {
+        let getDesc = (level) => " \\alpha_4=" + getA4(level).toString(0);
+        a4 = theory.createUpgrade(3, currency1, new ExponentialCost(BigNumber.from("ee14"), 1e5));
+        a4.getDescription = (_) => Utils.getMath(getDesc(a4.level));
+        a4.getInfo = (amount) => Utils.getMathTo(getDesc(a4.level), getDesc(a4.level + amount));
     }
     
     /////////////////////
@@ -72,8 +80,9 @@ var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.00
 var getTau = () => currency1.value;
 var get2DGraphValue = () => currency1.value.sign * (BigNumber.ONE + currency1.value.abs()).log10().toNumber();
 
-var getA1 = (level) => Utils.getStepwisePowerSum(level, 8, 2, 0) + BigNumber.from(getA2(a2.level));
+var getA1 = (level) => Utils.getStepwisePowerSum(level, 4, 3, 0) + BigNumber.from(getA2(a2.level));
 var getA2 = (level) => Utils.getStepwisePowerSum(level, 120, 2, 0) + BigNumber.from(getA3(a3.level));
-var getA3 = (level) => Utils.getStepwisePowerSum(level, 1e16, 2, 0);
+var getA3 = (level) => Utils.getStepwisePowerSum(level, 1e16, 2, 0) + BigNumber.from(getA4(a4.level));
+var getA3 = (level) => Utils.getStepwisePowerSum(level, 1e30, 2, 0);
 
 init();
