bdfb7955d90ba851e5e9cab392f7dd27dbea6e15
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-19 18:12:17 +0800

    Update Infinity-layers.js

--- OFFICAL/short/Infinity-layers.js
+++ OFFICAL/short/Infinity-layers.js
@@ -61,7 +61,7 @@ var getPrimaryEquation = () => {
 
 var getPublicationMultiplier = (tau) => tau.pow(0.001) / BigNumber.HUNDRED;
 var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.001}}{100}";
-var getTau = () => currency.value;
+var getTau = () => currency1.value;
 var get2DGraphValue = () => currency1.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 
 var getA1 = (level) => Utils.getStepwisePowerSum(level, 8, 2, 0) + BigNumber.from(a2.level * BigNumber.from("1e300"));
