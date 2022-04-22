d8cc8cdea59ce750b17d337b867fb1824aa85a4d
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-22 18:36:18 +0800

    Create AntimatterDimensions.js

---
+++ OFFICAL/AntimatterDimensions.js
@@ -0,0 +1,11 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "AD";
+var name = "Antimatter Dimensions";
+var description = "ad. AntimatterDimensionsAntimatterDimensionsAntimatterDimensionsAntimatterDimensionsAntimatterDimensions";
+var authors = "=)";
+var version = 1;
