8e7c55a005334f20b8182315a78d36a60cc20f05
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-22 18:41:38 +0800

    Update AntimatterDimensions.js

--- OFFICAL/AntimatterDimensions.js
+++ OFFICAL/AntimatterDimensions.js
@@ -9,3 +9,13 @@ var name = "Antimatter Dimensions";
 var description = "ad. AntimatterDimensionsAntimatterDimensionsAntimatterDimensionsAntimatterDimensionsAntimatterDimensions";
 var authors = "=)";
 var version = 1;
+
+// currency
+var Antimatter;
+
+// prestige
+var DB, AG;
+
+// normal dimensions
+var d1, d2, d3, d4, d5, d6, d7, d8;
+var md1, md2, md3, md4, md5, md6, md7, md8;
