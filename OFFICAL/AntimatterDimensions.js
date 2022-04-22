fafb986ef742b7e846a48708a6b0ae6aa17a8662
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-22 19:00:09 +0800

    Update AntimatterDimensions.js

--- OFFICAL/AntimatterDimensions.js
+++ OFFICAL/AntimatterDimensions.js
@@ -24,6 +24,15 @@ var d1, d2, d3, d4, d5, d6, d7, d8;           // |  bought dimension  |
 var dd1, dd2, dd3, dd4, dd5, dd6, dd7, dd8;   // |   auto  dimension  |
 var md1, md2, md3, md4, md5, md6, md7, md8;   // |   mult  dimension  |
 
+// challenge
+var C1, C2, C3, C4, C5, C6, C7, C8;           // |   normal challenge   |
+var IC1, IC2, IC3, IC4, IC5, IC6, IC7, IC8;   // |  infinity challenge  |
+
+C1 = C2 = C3 = C4 = C5 = C6 = C7 = C8 = false;
+IC1 = IC2 = IC3 = IC4 = IC5 = IC6 = IC7 = IC8 = false;
 
 
 
+
+var init = () => {
+    Antimatter = theory.createCurrency();
