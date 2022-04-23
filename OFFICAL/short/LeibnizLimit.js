b2f9e76cc066a4161b8b6d996d015297ff92fef2
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-23 19:49:03 +0800

    Create LeibnizLimit.js

---
+++ OFFICAL/short/LeibnizLimit.js
@@ -0,0 +1,13 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "LeibnizLimit";
+var name = "Leibniz Theory";
+var description = "A theory thats limit to pi.";
+var authors = "Skyhigh173";
+var version = 1;
+
+var k, 
