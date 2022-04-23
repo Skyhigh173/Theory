6424b4298e8566a5ead3d5c741816da4f1ce89d3
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-23 19:57:33 +0800

    Update LeibnizLimit.js

--- OFFICAL/short/LeibnizLimit.js
+++ OFFICAL/short/LeibnizLimit.js
@@ -10,4 +10,24 @@ var description = "A theory thats limit to pi.";
 var authors = "Skyhigh173";
 var version = 1;
 
-var k, 
+var k, q1, q2;
+
+
+
+/*
+function Limit (k) {
+  let result = 0;
+  for (let i = 0; i < k; i++) {
+    result += Pi(i);
+  }
+  return result;
+}
+
+function Pi (n) {
+  let upper = 2 * ((-1) ** (n)) * ((3) ** (1 / 2 - n));
+  let lower = 2 * n + 1;
+  return upper / lower;
+}
+
+console.log(Limit(100000));
+*/
