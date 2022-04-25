8e4875e66e1aff98fff8427046d7adecb9592b11
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-25 13:47:39 +0800

    Create Logistic_Map.js

---
+++ Logistic_Map.js
@@ -0,0 +1,14 @@
+import { ExponentialCost, FirstFreeCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "LGMP"
+var name = "Logistic Map";
+var description = "Logistic map, cool and good ouo";
+var authors = "Gilles-Philippe Paillé";
+var version = 1;
+
+
+var 
