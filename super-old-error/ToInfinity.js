43f70ed2589babe5197db49d8ef54d57f0295c29
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-04-28 15:36:07 +0800

    Create ToInfinity.js

---
+++ super-old-error/ToInfinity.js
@@ -0,0 +1,30 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "?";
+var name = "to Infinity!";
+var description = "huh";
+var authors = "nobody";
+var version = 1;
+
+
+var currency;
+
+var init = () => {
+    currency = theory.createCurrency();
+    currency.value = 2;
+}
+var tick = (elapsedTime, multiplier) => {
+    currency.value *= currency.vaule.pow(currency.value);
+}
+
+var getPrimaryEquation = () => "\\dot{\\rho} = TREE(3)";
+var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
+var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
+var getTau = () => currency.value;
+var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
+
+init;
