d76f375bd272df3ad61c3d9783e845fc288d1014
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-05-06 21:04:47 +0800

    Create ClickerAndIdle.js

---
+++ Sus/ClickerAndIdle.js
@@ -0,0 +1,13 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "while (sky == liver);";
+var name = "Grass Clicker";
+var description = "An implementation of... what? everything you buy will only works while offline";
+var authors = "Sky == liver";
+var version = 1;
+
+var currency;
