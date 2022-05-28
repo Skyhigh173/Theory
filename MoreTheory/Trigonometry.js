7636ceba481a242f3f48623402f627943e4cf847
Skyhigh173 <100467674+Skyhigh173@users.noreply.github.com>
2022-05-28 21:29:48 +0800

    Update Trigonometry.js

--- MoreTheory/Trigonometry.js
+++ MoreTheory/Trigonometry.js
@@ -68,7 +68,7 @@ var init = () => {
      // vdt
     {
         let getDesc = (level) => "\\vartheta =" + getDT(level).toString(0);
-        vdt = theory.createUpgrade(5, currency1, new ExponentialCost(25, Math.log2(20)));
+        vdt = theory.createUpgrade(5, currency1, new ExponentialCost(25, Math.log2(16)));
         vdt.getDescription = (_) => Utils.getMath(getDesc(vdt.level));
         vdt.getInfo = (amount) => Utils.getMathTo(getDesc(vdt.level), getDesc(vdt.level + amount));
     }
